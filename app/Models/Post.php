3808a6202e6d1d13e7a5e18df8a886195baae24a
Lucas Victore Zille <89852261+RivnatZille@users.noreply.github.com>
2022-03-09 16:11:12 -0300

    Pushing Section 2 classes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,17 +3,58 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
+    public $title;
+
+    public $excerpt;
+
+    public $date;
+
+    public $body;
+
+    public $slug;
+
+    /**
+     * @param $title
+     * @param $excerpt
+     * @param $date
+     * @param $body
+     * @param $slug
+     */
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
+    public static function all ()
+    {
+        return cache()->remember('posts.all', now()->addSeconds(5), function () {
+            return collect(File::files(resource_path("posts")))
+                ->map(function ($file) {
+                    return YamlFrontMatter::parseFile($file);
+                })
+                ->map(function ($document) {
+                    return new Post(
+                        $document->title,
+                        $document->excerpt,
+                        $document->date,
+                        $document->body(),
+                        $document->slug
+                    );
+                })->sortByDesc('date');
+        });
+    }
+
     public static function find ($slug)
     {
-        if (! file_exists($path = $path = resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{slug}", 1200, function () use ($path) {
-            return file_get_contents($path);
-        });
+        return static::all()->firstWhere('slug', $slug);
     }
 }
