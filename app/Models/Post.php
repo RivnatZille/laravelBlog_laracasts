4031212c0d573076a906dacf59c1cd8f23e6f9cb
Lucas Victore Zille <89852261+RivnatZille@users.noreply.github.com>
2022-03-11 17:31:53 -0300

    Changes to the posts table, added slug column

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // Fillable properties are used for when you want to mass assign values to an Object and you want to specify
+    // which ones are mass assignable. On the example below, I can mass assign a title, an excerpt and the body
+//    protected $fillable = ['title', 'excerpt', 'body'];
+
+    // Guarded properties are used for when you want to mass assign values to an Object and you want to specify which
+    // ones aren't mass assignable. On the example below, I can mass assign every value, except the title
+//    protected $guarded = ['title'];
+
+    // Also, you can disable mass assignment completely, but declaring a blank guarded property, just like below
+//    protected $guarded = [];
 }
