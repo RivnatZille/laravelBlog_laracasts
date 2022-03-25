47328766d9b8997d957463591fbc0b80a47a87f3
RivnatZille <lucasvzille@hotmail.com>
2022-03-25 17:31:04 -0300

    Improvements on the Register User features

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -15,29 +16,35 @@ class RegisterController extends Controller
     public function store()
     {
         $attributes = request()->validate([
+//            'name' => 'required|max:255',
+//            'username' => 'required|min:3|max:255|unique:users,username',
+//            'email' => 'required|email|max:255|unique:users,email',
+//            'password' => 'required|min:7|max:255'
             'name' => [
                 'required',
                 'max:255'
             ],
             'username' => [
                 'required',
+                'min:3',
                 'max:255',
-                'min:3'
+                Rule::unique('users', 'username')
             ],
             'email' => [
                 'required',
                 'email',
-                'max:255'
+                'max:255',
+                Rule::unique('users', 'email')
             ],
             'password' => [
                 'required',
-                'max:255',
-                'min:7'
+                'min:7',
+                'max:255'
             ]
         ]);
 
         User::create($attributes);
 
-        return redirect('/');
+        return redirect('/')->with('success', 'Your account has been created.');
     }
 }
